6ed39d98f32534512a85a282bde4a04d4239ca79
P1ntas <72892065+P1ntas@users.noreply.github.com>
2023-10-14 02:10:03 +0100

    Done

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -26,7 +26,7 @@ export const Contact = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     setButtonText("Sending...");
-    let response = await fetch("http://localhost:5000/contact", {
+    let response = await fetch("http://localhost:5001/contact", {
       method: "POST",
       headers: {
         "Content-Type": "application/json;charset=utf-8",
@@ -73,7 +73,7 @@ export const Contact = () => {
                     <Col size={12} sm={6} className="px-1">
                       <input type="tel" value={formDetails.phone} placeholder="Phone No." onChange={(e) => onFormUpdate('phone', e.target.value)}/>
                     </Col>
-                    <Col size={12} className="px-1">
+                    <Col size={12} className="px-1 textarea">
                       <textarea rows="6" value={formDetails.message} placeholder="Message" onChange={(e) => onFormUpdate('message', e.target.value)}></textarea>
                       <button type="submit"><span>{buttonText}</span></button>
                     </Col>
