b0b29bb6611332248e6e94261649d76816b2e442
P1ntas <72892065+P1ntas@users.noreply.github.com>
2023-10-14 01:13:12 +0100

    only mail missing

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,11 +1,9 @@
-import meter1 from "../assets/img/meter1.svg";
-import meter2 from "../assets/img/meter2.svg";
-import meter3 from "../assets/img/meter3.svg";
+import meter1 from "../assets/img/meter80.png";
+import meter2 from "../assets/img/meter85.png";
+import meter3 from "../assets/img/meter90.png";
+import meter4 from "../assets/img/meter95.png";
 import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
-import arrow1 from "../assets/img/arrow1.svg";
-import arrow2 from "../assets/img/arrow2.svg";
-import colorSharp from "../assets/img/color-sharp.png"
 
 export const Skills = () => {
   const responsive = {
@@ -43,23 +41,23 @@ export const Skills = () => {
                                 <h5>Frontend</h5>
                             </div>
                             <div className="item">
-                                <img src={meter2} alt="Image" />
+                                <img src={meter1} alt="Image" />
                                 <h5>Backend</h5>
                             </div>
                             <div className="item">
-                                <img src={meter3} alt="Image" />
+                                <img src={meter2} alt="Image" />
                                 <h5>Databases</h5>
                             </div>
                             <div className="item">
-                                <img src={meter1} alt="Image" />
+                                <img src={meter2} alt="Image" />
                                 <h5>Communication</h5>
                             </div>
                             <div className="item">
-                                <img src={meter1} alt="Image" /> 
+                                <img src={meter3} alt="Image" /> 
                                 <h5>Adaptability</h5>
                             </div>
                             <div className="item">
-                                <img src={meter1} alt="Image" />
+                                <img src={meter4} alt="Image" />
                                 <h5>Attention to Detail</h5>
                             </div>
                         </Carousel>
